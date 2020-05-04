5217f3d1c4e5717092d77dd6ed06f50742707313
sophi_4 (mac) <sophi_4@github.com>
2020-05-04 09:06:52 +0100

    updated - included sketch js file

--- proxy.js
+++ proxy.js
@@ -1,15 +1,18 @@
 /* Websocket to serial proxy. */
 
-var app = require('express')();
+const express = require('express');
+const app = express ();
+
+//var app = require('express')();
+
+var five = require("johnny-five");
+
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var SerialPort = require('serialport');
 
 const port = new SerialPort("/dev/cu.usbmodem14641");
 
-//const app = express ();
-
-var five = require("johnny-five");
 
 //code for the board, initializes colour when board is one    
 five.Board().on('ready',function(){
@@ -40,25 +43,7 @@ port.open(function (err) {
     }
 });
 
-    app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-    });
 
-    socket.on('message', function(msg) {
-        console.log(new Date());
-        //console.log('message: ' + msg);
-        port.write(msg);
-    });
-
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-
-    port.on('data', function (data) {
-        //console.log('Data:', data);
-        socket.emit('arduino', data[0]);
-    });
-    
     http.listen(3000, function() {
     console.log('listening on *:3000');
     });
---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup () {
+    createCanvas (300, 300);
+}
+
+function draw () {
+ background (51);
+    ellipse (mouseX, mouseY, 60, 60);
+}
